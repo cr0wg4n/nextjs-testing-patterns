6110e5b3e677ac0a5263fad23f89603e24c1b0fc
Mauricio M <mcm.crw@gmail.com>
2023-12-14 00:43:23 -0400

    feat, production ready

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    reactRemoveProperties: { 
+      properties: process.env.NODE_ENV === 'production' ? 
+        ['^data-testid$', '^data-cy$'] : 
+        []
+    }
+  }
+}
 
 module.exports = nextConfig
